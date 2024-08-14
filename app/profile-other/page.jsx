1dc6ef07f43fd26587f5b2fd2aece9b1e7f8a0a6
Lining TANG <liningtang@linings-macbook-pro.home>
2024-08-14 21:17:30 +0200

    fix typo

--- app/profile-other/page.jsx
+++ app/profile-other/page.jsx
@@ -1,12 +1,12 @@
 'use client';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Suspense } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Profile from "@components/Profile";
 
-const UserProfile = () => {
+const UserProfileContent = () => {
     const { data: session } = useSession();
     const searchParams = useSearchParams();
     const router = useRouter();
@@ -24,22 +24,34 @@ const UserProfile = () => {
         }
 
         if (creatorId) fetchPosts();
-    }, [])
+    }, [creatorId]);
+
+    if (!creatorId || !creatorName) {
+        return <div>Loading...</div>;
+    }
+
+    if (session?.user && session?.user.name.replace(" ", "").toLowerCase() === creatorName) {
+        router.push(`/profile`);
+        return null; // Prevent rendering anything if redirecting
+    }
 
     return (
-        session?.user && session?.user.name.replace(" ", "").toLowerCase() === creatorName ? (
-            router.push(`/profile`)
-        )
-            : (
-                <Profile
-                    name={creatorName}
-                    desc=""
-                    data={posts}
-                    handleEdit={() => { }}
-                    handleDelete={() => { }}
-                />
-            )
-    )
-}
+        <Profile
+            name={creatorName}
+            desc=""
+            data={posts}
+            handleEdit={() => { }}
+            handleDelete={() => { }}
+        />
+    );
+};
+
+const UserProfile = () => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <UserProfileContent />
+        </Suspense>
+    );
+};
 
 export default UserProfile;
